2f7f7367e9c252da8210f6e2853f0fff732568e9
stormckey <chenhaogao123@gmail.com>
2025-02-25 11:35:03 +0100

    fix: fix problems in accipit interpreter

--- ir/accipit.py
+++ ir/accipit.py
@@ -7,16 +7,22 @@ from typing import Union, Any
 import sys
 import argparse
 
+sys.setrecursionlimit(50000)  # 加深调用栈深度，因为python没有尾递归优化
+
+
 class SemanticError(Exception):
     def __init__(self, message):
         super().__init__(message)
-        
+
+
 STEP = 0
 DEBUG = False
+
 
 class IRNode():
     wrap = True
     return_msg = ""
+
     def method_wrapper(self, func, step: int):
         def wrapper(*args, **kwargs):
             name = self.__str__().split('\n')[0].strip().rstrip("{").strip()
@@ -27,7 +33,7 @@ class IRNode():
             print(f"{prefix} {msg}")
             return result
         return wrapper
-    
+
     def __getattribute__(self, name):
         obj = super().__getattribute__(name)
         if name == "eval" and self.wrap:
@@ -41,63 +47,75 @@ class IRNode():
     def __str__(self):
         return self.__class__.__name__ + " " + " ".join(f"({attr})" for attr in self.__dict__.values()) + " "
 
+
 @dataclass
 class IntConst(Ast):
     value: int
-    
+
     def __str__(self):
         return str(self.value)
-    
+
     def eval(self) -> int:
         return self.value
-    
+
+
 @dataclass
 class NoneConst():
     def eval(self):
         return 1
-    
+
+
 @dataclass
 class UnitConst():
     def eval(self) -> UnitConst:
         return self
-    
-@dataclass 
+
+
+@dataclass
 class Ident(IRNode):
     name: str
-    
+
     def __str__(self):
         return self.name
-    
+
     def eval(self) -> Any:
         return env.get(self)
-    
+
+
 Value = Union[IntConst, NoneConst, UnitConst, Ident]
+
 
 class I32(IRNode):
     def __str__(self):
         return "i32"
 
+
 class Unit(IRNode):
     def __str__(self):
         return "()"
-    
+
+
 @dataclass
 class Pointer(IRNode):
     name: str
+
 
 @dataclass
 class FunType(IRNode):
     params: list[Token]
     ret: Token
-    
+
     def __str__(self):
         return f"fn ({', '.join(str(param) for param in self.params)}) -> {self.ret}"
-    
+
+
 Type = Union[I32, Unit, Pointer, FunType]
+
 
 @dataclass
 class Ptr(IRNode):
     addr: int
+
 
 class Environment():
     def __init__(self):
@@ -106,13 +124,13 @@ class Environment():
         self.memory: list[int] = [0]*1024
         self.capacity: int = 1024
         self.size: int = 0
-        
+
     def push_frame(self):
         self.frames.append({})
 
     def pop_frame(self):
         self.frames.pop()
-        
+
     def allocate(self, size: int, init: list[int] = []) -> Ptr:
         if self.size + size > self.capacity:
             size_lacked = self.size + size - self.capacity
@@ -130,47 +148,53 @@ class Environment():
         if name in self.global_env:
             raise SemanticError(f"Global identifier {name} is defined twice.")
         self.global_env[name] = value
-        
+
     def get_global(self, name) -> Any:
         return self.global_env.get(name.__str__())
-        
+
     def add_local(self, name, value: Any):
         name = name.__str__()
         self.frames[-1][name] = value
-        
+
     def get_local(self, name) -> Any:
         return self.frames[-1].get(name.__str__())
-        
+
     def get(self, name: Ident) -> Any:
         if name.name.startswith("@"):
             return self.get_global(name)
         else:
             return self.get_local(name)
-        
+
     def store(self, name: Ident, value: int):
         ptr = self.get(name)
+        if ptr is None:
+            raise SemanticError(f"{name} is not defined.")
         if not isinstance(ptr, Ptr):
             raise SemanticError(f"{name} is not a pointer.")
         self.memory[ptr.addr] = value
-        
+
     def load(self, name: Ident) -> int:
         ptr = self.get(name)
+        if ptr is None:
+            raise SemanticError(f"{name} is not defined.")
         if not isinstance(ptr, Ptr):
             raise SemanticError(f"{name} is not a pointer.")
         return self.memory[ptr.addr]
-        
+
     def clear(self):
         self.global_env.clear()
         self.frames.clear()
 
+
 env = Environment()
+
 
 @dataclass
 class BinExpr(IRNode, Ast):
     binop: Token
     v1: IntConst | Ident
     v2: IntConst | Ident
-    
+
     def eval(self):
         v1 = self.v1.eval()
         v2 = self.v2.eval()
@@ -204,30 +228,34 @@ class BinExpr(IRNode, Ast):
             return v1 >= v2
         else:
             raise SemanticError(f"Unknown binop {self.binop}")
-    
+
+
 @dataclass
 class Alloca(IRNode, Ast):
     tpe: Type
     size: IntConst
-    
+
     def eval(self) -> Ptr:
         return env.allocate(self.size.eval())
+
 
 @dataclass
 class Load(IRNode, Ast):
     name: Ident
-    
+
     def eval(self):
         return env.load(self.name)
+
 
 @dataclass
 class Store(IRNode, Ast):
     wrap = False
     value: Ident
     name: Ident
-    
+
     def eval(self):
         env.store(self.name, self.value.eval())
+
 
 @dataclass
 class Gep(IRNode):
@@ -238,14 +266,15 @@ class Gep(IRNode):
     def __str__(self):
         indexing = ", ".join(f"{idx} < {dim}" for idx, dim in self.offsets)
         return f"offset {self.tpe}, {self.name}, {indexing}"
-    
+
     def eval(self) -> Ptr:
         addr = env.get(self.name).addr
         p_addr = 0
         for idx, dim in self.offsets:
             p_addr = p_addr * dim.eval() + idx.eval()
         return Ptr(addr + p_addr)
-    
+
+
 @dataclass
 class Fncall(IRNode):
     name: Ident
@@ -253,7 +282,7 @@ class Fncall(IRNode):
 
     def __str__(self):
         return f"call {self.name} ({', '.join(str(arg) for arg in self.args)})"
-    
+
     def eval(self):
         if self.name.__str__() == "@write":
             print(self.args[0].eval())
@@ -262,48 +291,54 @@ class Fncall(IRNode):
             return int(input())
         fun = env.get_global(self.name)
         if not isinstance(fun, FunDefn) and not isinstance(fun, FunDecl):
-            raise SemanticError(f"{self.name} is not a function.") 
+            raise SemanticError(f"{self.name} is not a function.")
         return fun.eval(self.args)
 
+
 ValueBindingOp = Union[BinExpr, Gep, Fncall, Alloca, Load, Store]
+
 
 @dataclass
 class ValueBinding(IRNode):
     name: Ident
     op: ValueBindingOp
-    
+
     def __str__(self):
         return f"\tlet {self.name} = {self.op}"
-    
+
     def eval(self):
         value = self.op.eval()
         env.add_local(self.name, value)
         self.return_msg = f"\033[1;33mBind  \033[0m {value} to {self.name}"
-        
+
+
 @dataclass
 class Br(IRNode, Ast):
     cond: Value
     label1: Ident
     label2: Ident
-    
+
     def eval(self) -> int:
         target = self.label1 if self.cond.eval() else self.label2
         return env.get(target).eval()
-    
+
+
 @dataclass
 class Jmp(IRNode, Ast):
     label: Ident
-    
+
     def eval(self) -> int:
         return env.get(self.label).eval()
+
 
 @dataclass
 class Ret(IRNode):
     value: IntConst | Ident | UnitConst
-    
+
     def eval(self) -> Value:
         return self.value.eval()
-    
+
+
 Terminator = Union[Br, Jmp, Ret]
 
 
@@ -311,80 +346,89 @@ Terminator = Union[Br, Jmp, Ret]
 class PList(IRNode):
     wrap = False
     params: list[tuple[Ident, Type]]
-    
+
     def __str__(self):
         return "(" + ", ".join(f"{name}: {tpe}" for name, tpe in self.params) + ")"
-    
+
     def eval(self, values: list[Ident | IntConst]):
         values = [value.eval() for value in values]
         env.push_frame()
         for (name, _), value in zip(self.params, values):
             env.add_local(name, value)
-    
+
+
 @dataclass
 class BasicBlock(IRNode):
     wrap = False
     label: Ident
-    bindings: list[ValueBinding]
-    terminator: Terminator
-    
+    insts: list[ValueBinding | Terminator]
+
     def __str__(self):
         return f"{self.label}:\n" + "\n".join(str(binding) for binding in self.bindings) + f"\n{self.terminator}"
-    
+
     def eval(self) -> int | UnitConst:
-        for binding in self.bindings:
-            binding.eval()
-        return self.terminator.eval()
-    
+        for idx, inst in enumerate(self.insts):
+            if isinstance(inst, ValueBinding):
+                inst.eval()
+            else:
+                if idx != len(self.insts) - 1:
+                    raise SemanticError(
+                        f"Terminator instruction {inst} is not the last instruction in block {self.label}.")
+                return inst.eval()
+
+
 @dataclass
 class Body(IRNode):
     wrap = False
     bbs: list[BasicBlock]
-    
+
     def __str__(self):
         return "{\n" + "\n".join(str(bb) for bb in self.bbs) + "\n}"
-    
+
     def eval(self) -> int:
         return self.bbs[0].eval()
-    
+
+
 class GlobalDecl:
     name: Ident
     tpe: Type
     size: IntConst
     values: list[IntConst]
-    
+
     def __init__(self, name: Ident, tpe: Type, size: IntConst, values: list[IntConst]):
         self.name = name
         self.tpe = tpe
         self.size = size
         self.values = values
         if values and len(values) != size.eval():
-            raise SemanticError(f"Global array {name} has size {size} but {len(values)} values are provided.")
+            raise SemanticError(
+                f"Global array {name} has size {size} but {len(values)} values are provided.")
         ptr = env.allocate(size.eval(), [value.eval() for value in values])
         env.add_global(name, ptr)
-    
+
     def __str__(self):
         if self.values:
             return f"{self.name} : {self.tpe}, {self.size} = [{', '.join(str(value) for value in self.values)}]"
         else:
             return f"{self.name} : {self.tpe}, {self.size}"
-        
+
+
 class FunDefn(IRNode, Ast):
     name: Ident
     params: PList
     ret: Type
     body: Body
-    
+
     def __init__(self, name: Ident, params: PList, ret: Type, body: Body):
         self.name = name
         self.params = params
         self.ret = ret
         self.body = body
         env.add_global(name, self)
-    
+
     def __str__(self):
         return f"fn {self.name} ({self.params}) -> {self.ret} {self.body}"
-    
+
     def eval(self, args: list[Value]) -> int:
         self.params.eval(args)
         for bb in self.body.bbs:
@@ -392,52 +436,64 @@ class FunDefn(IRNode, Ast):
         return_value = self.body.eval()
         env.pop_frame()
         return return_value
-    
-    
+
+
 class FunDecl(IRNode, Ast):
     name: Ident
     params: PList
     ret: Type
-    
+
     def __init__(self, name: Ident, params: PList, ret: Type):
         self.name = name
         self.params = params
         self.ret = ret
         env.add_global(name, self)
-    
+
     def __str__(self):
         return f"fn {self.name} ({self.params}) -> {self.ret};"
-    
+
+
 Decl = Union[GlobalDecl, FunDefn, FunDecl]
+
 
 @dataclass
 class Program():
     decls: list[Decl]
-    
+
     def __str__(self):
         return "\n".join(str(decl) for decl in self.decls)
 
+
 class BaseTransformer(Transformer):
-    name = lambda _, children: "".join(children)
-    i32 = lambda _, _token: I32()
-    unit = lambda _, _token: Unit()
-    int_const = lambda _, n: IntConst(int(n[0]))
-    none_const = lambda _, _token: NoneConst()
-    unit_const = lambda _, _token: UnitConst()
-    SIGNED_INT = lambda _, n: int(n)
-    function_type = lambda _, items: FunType(items[:-1], items[-1])
-    pointer = lambda _, items: Pointer(items[0].__str__() + "*")
-    ident = lambda _, items: Ident(items[0] + items[1])
-    gep = lambda _, items: Gep(items[0], items[1], [(items[i], items[i+1]) for i in range(2, len(items), 2)])
-    fncall = lambda _, items: Fncall(items[0], items[1:])
-    value_binding_untyped = lambda _, items: ValueBinding(items[0], items[1])
-    value_binding_typed = lambda _, items: ValueBinding(items[0], items[2])
-    ret = lambda _, items: Ret(items[1])
-    plist = lambda _, items: PList([(items[i], items[i+1]) for i in range(0, len(items), 2)])
-    bb = lambda _, items: BasicBlock(items[0], items[1:-1], items[-1])
-    body = lambda _, items: Body(items)
-    global_decl = lambda _, items: GlobalDecl(items[0], items[1], items[2], items[3:])
-    program = lambda _, items: Program(items)
+    def name(_, children): return "".join(children)
+    def i32(_, _token): return I32()
+    def unit(_, _token): return Unit()
+    def int_const(_, n): return IntConst(int(n[0]))
+    def none_const(_, _token): return NoneConst()
+    def unit_const(_, _token): return UnitConst()
+    def SIGNED_INT(_, n): return int(n)
+    def function_type(_, items): return FunType(items[:-1], items[-1])
+    def pointer(_, items): return Pointer(items[0].__str__() + "*")
+    def ident(_, items): return Ident(items[0] + items[1])
+    def gep(_, items): return Gep(items[0], items[1], [
+        (items[i], items[i+1]) for i in range(2, len(items), 2)])
+
+    def fncall(_, items): return Fncall(items[0], items[1:])
+    def value_binding_untyped(
+        _, items): return ValueBinding(items[0], items[1])
+
+    def value_binding_typed(_, items): return ValueBinding(items[0], items[2])
+    def ret(_, items): return Ret(items[1])
+    def plist(_, items): return PList(
+        [(items[i], items[i+1]) for i in range(0, len(items), 2)])
+
+    def bb(_, items): return BasicBlock(items[0], items[1:])
+    def body(_, items): return Body(items)
+    def global_decl(_, items): return GlobalDecl(
+        items[0], items[1], items[2], items[3:])
+
+    def program(_, items): return Program(items)
+
 
 accipit_grammar = """
     ?start : program
@@ -478,7 +534,7 @@ accipit_grammar = """
     
     ?plist : (ident ":" type ("," ident ":" type)*)?
     ?label : ident ":"
-    ?bb : label (value_binding| terminator)*
+    ?bb : label (value_binding| terminator)+
     body : "{" bb* "}"
     
     global_decl : ident ":" "region" type "," int_const ("=" "[" value ("," value)* "]")?
@@ -498,24 +554,30 @@ accipit_grammar = """
 """
 
 this_module = sys.modules[__name__]
-accipit_transformer = ast_utils.create_transformer(this_module, BaseTransformer())
-parser = Lark(accipit_grammar, parser="lalr", transformer=accipit_transformer)
+accipit_transformer = ast_utils.create_transformer(
+    this_module, BaseTransformer())
+parser = Lark(accipit_grammar)
+
 
 def parse(file: str) -> Program:
     with open(file) as f:
         text = f.read()
     try:
-        result: Program = parser.parse(text)
+        ast = parser.parse(text)
+        result: Program = accipit_transformer.transform(ast)
         return result
     except UnexpectedInput as e:
         print(e.get_context(text))
         print(f"Syntax error at position {e.column}: {e}")
         exit(1)
 
+
 if __name__ == "__main__":
-    arg_parser = argparse.ArgumentParser(description="Interpreter for Accipit IR")
+    arg_parser = argparse.ArgumentParser(
+        description="Interpreter for Accipit IR")
     arg_parser.add_argument("file", type=str, help="the IR file to interpret.")
-    arg_parser.add_argument("-d", "--debug", action="store_true", help="whether to print debug info.")
+    arg_parser.add_argument(
+        "-d", "--debug", action="store_true", help="whether to print debug info.")
     args = arg_parser.parse_args()
     program = parse(args.file)
     if args.debug:
